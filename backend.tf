5da4276c3866cc68221b347e12a96a5857432822
mohansgithub <mohanraj123@gmail.com>
2025-11-15 07:56:47 +0530

    Add DynamoDB table for state locking

--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@
     backend "s3" {
         bucket = "mohanraj-terraform-states"
         key    = "terraform/state/terraform.tfstate"
+        dynamodb_table = "terraform-lock-table"
         region = "ap-south-1"
     }
    
